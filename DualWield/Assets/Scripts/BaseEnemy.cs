7d05d45f1069036aae56eef15fc56a8eb5d2ecf9
Justin Tran <jhtran023@gmail.com>
2024-03-28 17:37:19 +1100

    Implement enemy follows player

--- DualWield/Assets/Scripts/BaseEnemy.cs
+++ DualWield/Assets/Scripts/BaseEnemy.cs
@@ -6,19 +6,39 @@ public class BaseEnemy : BaseCharacter
 {
     protected GameObject enemyModel;
     protected EnemyTypes enemyType;
-    protected float attackType;
+    protected float speed = 1f;
+    protected float attackRange;
     protected float fieldOfView;
+
+    public LayerMask playerMask = 6;
+    public LayerMask obstructionMask = 7;
+    private Transform playerTransform;
+    private bool isPlayerSeen;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        try
+        {
+            playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        }
+        catch
+        {
+            Debug.Log("Player cannot be found");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveTowardsTarget();
+    }
+    private void MoveTowardsTarget()
+    {
+        if (playerTransform != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
     }
 }
 
