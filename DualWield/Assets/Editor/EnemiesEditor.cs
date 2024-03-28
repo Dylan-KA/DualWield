ed7e19409669c477145702421dc2bbe73a1930db
Justin Tran <jhtran023@gmail.com>
2024-03-28 21:48:29 +1100

    Add base enemy field of view, rotate model and attack speed/attack message

---
+++ DualWield/Assets/Editor/EnemiesEditor.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BaseEnemy))]
+public class EnemiesEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        BaseEnemy fov = (BaseEnemy)target;
+        Handles.color = Color.blue;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.GetViewDistance());
+
+        Vector3 viewAngle01 = DirectionFromFieldOfView(fov.transform.eulerAngles.y, -fov.GetFieldOfView() / 2);
+        Vector3 viewAngle02 = DirectionFromFieldOfView(fov.transform.eulerAngles.y, fov.GetFieldOfView() / 2);
+
+        Handles.color = Color.yellow;
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.GetViewDistance());
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.GetViewDistance());
+    }
+
+    private Vector3 DirectionFromFieldOfView(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
