2bbab6a8d0e9bf26e316612bc756c299aa24ed0c
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2024-04-06 11:55:14 +1100

    Fixed falling physics (again)

--- DualWield/Assets/Scripts/PlayerCharacter.cs
+++ DualWield/Assets/Scripts/PlayerCharacter.cs
@@ -18,6 +18,7 @@ public class PlayerCharacter: BaseCharacter
     public Camera playerCamera;
     public float gravity = 10f;
     private Vector3 velocity;
+    private float maxFallingSpeed = -50.0f;
 
     public float lookspeed = 2f;
     public float lookXLimit = 90f;
@@ -85,12 +86,7 @@ public class PlayerCharacter: BaseCharacter
             rightWeapon.SetOtherWeaponType(leftWeapon.GetWeaponType());
         }
 
-       
-        
-        
     }
-
-
     
     void Update()
     {
@@ -101,14 +97,23 @@ public class PlayerCharacter: BaseCharacter
         IsGrounded();
         if (!isFlying)
         {
-            velocity.y -= gravity * Time.deltaTime ;
-            if (velocity.y <= -50f)
+            if (!isGrounded)
             {
-                velocity.y = -50f;
+                velocity.y -= gravity * Time.deltaTime;
+
+                // limit to maximum falling speed 
+                if (velocity.y <= maxFallingSpeed)
+                {
+                    velocity.y = maxFallingSpeed;
+                }
             }
-            
+            else
+            {
+                velocity.y = 0;
+            }
+
         }
-        
+
         characterController.Move(velocity * Time.deltaTime);
         characterController.Move(moveDirection * Time.deltaTime);
         if (canMove)
@@ -130,6 +135,7 @@ public class PlayerCharacter: BaseCharacter
             //Debug.Log("Not Firing");
             leftWeapon.SetFiring(false);
             rightWeapon.SetFiring(false);
+            isFlying = false;
         }
         else
         {
@@ -146,6 +152,6 @@ public class PlayerCharacter: BaseCharacter
 
     public void IsGrounded()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 2);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 2, groundlayer);
     }
 }
