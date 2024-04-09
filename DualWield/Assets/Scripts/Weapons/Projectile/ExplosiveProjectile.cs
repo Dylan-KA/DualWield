ad0695de8c3aff738551b89206087ab5a2787b0e
dadysherwin1 <86547852+dadysherwin1@users.noreply.github.com>
2024-04-10 03:09:51 +1000

    Added Rocket+Freeze & Rocket+Rocket & Rocket+Wind

--- DualWield/Assets/Scripts/Weapons/Projectile/ExplosiveProjectile.cs
+++ DualWield/Assets/Scripts/Weapons/Projectile/ExplosiveProjectile.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ExplosiveProjectile : Projectile
 {
     [SerializeField] private float explosiveRange;
-    [SerializeField] private float maxDamage;
 
     protected override void OnImpact(Collider _)
     {
@@ -16,10 +15,22 @@ public class ExplosiveProjectile : Projectile
             Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosiveRange, layerMask);
             foreach (Collider collider in hitColliders)
             {
-                Debug.Log(collider.gameObject.name);
                 BaseEnemy enemy = collider.gameObject.GetComponent<BaseEnemy>();
-                enemy.TakeDamage(maxDamage);
-                //enemy.TakeDamage();
+                float distance =  (transform.position - enemy.transform.position).magnitude - enemy.transform.localScale.magnitude;
+                distance = Mathf.Clamp(distance, 0, explosiveRange);
+                
+                float damagePercent;
+                if (distance < explosiveRange/2f)
+                    // deal full damage
+                    damagePercent = 1;
+                    
+                else
+                    // deal fractional damage
+                    damagePercent = ((explosiveRange - distance)/2f) / (explosiveRange/2f);
+
+                enemy.TakeDamage(baseDamage * damagePercent);
+                if (gameObject.name == "RocketFreeze(Clone)") 
+                    enemy.TemperatureChange(damagePercent * -5);
             }
         }
     }
