43e8bd3b75399fe5a354f21f965e39a0ecb061ab
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2024-04-14 16:08:38 +1000

    Tuning Weapon damage & ammo consumption

--- DualWield/Assets/Scripts/Weapons/Flamethrower.cs
+++ DualWield/Assets/Scripts/Weapons/Flamethrower.cs
@@ -9,12 +9,18 @@ using UnityEngine.PlayerLoop;
 public class Flamethrower : ParticleWeapon
 {
     [SerializeField] private float baseDamage;
-    [SerializeField] private float flameAndFlameMultiplier;
+    [SerializeField] private float damageMultiplier = 1.0f;
+    private float maxMultiplier = 5.0f;
+    private float muliplierIncreaseRate = 20.0f;
+
+    [SerializeField] private List<Color> defaultParticleColors;
+    [SerializeField] private List<Color> BlueFlameColors;
 
     protected override void Start()
     {
         
         base.Start();
+        damageMultiplier = 1.0f;
     }
 
     protected override void Update()
@@ -67,9 +73,10 @@ public class Flamethrower : ParticleWeapon
         if(ListofEnemies.Length != 0){
             foreach (BaseEnemy Enemy in ListofEnemies)
             {
-                Enemy.TakeDamage(baseDamage * flameAndFlameMultiplier * Time.deltaTime);
+                Enemy.TakeDamage(baseDamage * damageMultiplier * Time.deltaTime);
             }
         }
+        IncreaseMultiplier();
     }
     private void FlameAndWind()
     {
@@ -110,4 +117,45 @@ public class Flamethrower : ParticleWeapon
         }
     }
 
+    private void IncreaseMultiplier()
+    {
+        if (damageMultiplier >= maxMultiplier) { damageMultiplier = maxMultiplier; }
+        damageMultiplier += (muliplierIncreaseRate * Time.deltaTime);
+    }
+
+    public void RestMultiplier() { damageMultiplier = 1.0f; }
+
+    /*
+    private void BlueDamageFlame()
+    {
+        if (defaultParticleColors == null) { PopulateDefaultColours(); }
+        float clampedMultiplier = damageMultiplier / 5;
+        Color parentLerpCol = Color.Lerp(defaultParticleColors[0], BlueFlameColors[0], clampedMultiplier);
+        WeaponParticles.startColor = parentLerpCol;
+        ParticleSystem[] ChildParticles = WeaponParticles.GetComponentsInChildren<ParticleSystem>();
+        for (int i = 1; i < ChildParticles.Length; i++)
+        {
+            Color lerpedColor = Color.Lerp(defaultParticleColors[i], BlueFlameColors[i], clampedMultiplier);
+            ChildParticles[i].startColor = lerpedColor;
+        }
+    }
+
+    private void ResetBlueFlame()
+    {
+        if (defaultParticleColors == null) { PopulateDefaultColours(); }
+        WeaponParticles.startColor = defaultParticleColors[0];
+        ParticleSystem[] ChildParticles = WeaponParticles.GetComponentsInChildren<ParticleSystem>();
+        for (int i = 1; i < ChildParticles.Length; i++)
+        {
+            ChildParticles[i].startColor = defaultParticleColors[i];
+        }
+    }
+
+    private void PopulateDefaultColours()
+    {
+        defaultParticleColors.Add(WeaponParticles.startColor);
+        ParticleSystem[] ChildParticles = WeaponParticles.GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem particle in ChildParticles) { defaultParticleColors.Add(particle.startColor); }
+    }
+    */
 }
