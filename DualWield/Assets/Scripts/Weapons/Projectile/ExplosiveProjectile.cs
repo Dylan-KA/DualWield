85c6ca72f7753490bf43b4170b642caaa1822cd6
dadysherwin1 <86547852+dadysherwin1@users.noreply.github.com>
2024-04-22 03:21:36 +1000

    Fixed Rocket + Freeze

--- DualWield/Assets/Scripts/Weapons/Projectile/ExplosiveProjectile.cs
+++ DualWield/Assets/Scripts/Weapons/Projectile/ExplosiveProjectile.cs
@@ -30,7 +30,7 @@ public class ExplosiveProjectile : Projectile
 
                 enemy.TakeDamage(baseDamage * damagePercent);
                 if (gameObject.name == "RocketFreeze(Clone)") 
-                    enemy.AddFreezePercent(damagePercent * -5);
+                    enemy.AddFreezePercent(damagePercent * 50f);
             }
         }
     }
