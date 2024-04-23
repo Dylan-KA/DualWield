6b695bc6d98bdf3df907b4c51a43351871b41de6
Justin Tran <jhtran023@gmail.com>
2024-04-23 21:54:33 +1000

    Arena manager with start, next wave, toggle does and check for alive enemies

---
+++ DualWield/Assets/Scripts/Arena/ArenaTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaTrigger : MonoBehaviour
+{
+    [SerializeField] private WaveManager arena;
+
+    public void Start()
+    {
+        if (arena == null) Debug.LogError("Arena is not assigned");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        arena.StartWave();
+        Destroy(this.gameObject);
+    }
+}
---
+++ DualWield/Assets/Scripts/Arena/WaveManager.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> doorList = new();
+    [SerializeField] private List<GameObject> waveList = new();
+    private bool isArenaStarted = false;
+    private int currentWaveCount = 0;
+    private int maxWave = 0;
+
+    public int CurrentWaveCount { get => currentWaveCount+1; }
+    public int MaxWave { get => maxWave; }
+
+    public void Start()
+    {
+        SetUpWaveManager();
+    }
+
+    private void SetUpWaveManager()
+    {
+        currentWaveCount = 0;
+        maxWave = waveList.Count;
+        DisableAllWaves();
+    }
+
+    private void Update()
+    {
+        if (isArenaStarted == true)
+        {
+            if (IsWaveFinished())
+            {
+                NextWave();
+            }
+        }
+    }
+
+    public void StartWave()
+    {
+        isArenaStarted = true;
+        EnableCurrentWave();
+        ToggleDoor();
+    }
+
+    private void NextWave()
+    {
+        currentWaveCount++;
+        if (IsArenaFinished())
+        {
+            EndArena();
+        }
+        else
+        {
+            EnableCurrentWave();
+        }
+    }
+
+    private void EndArena()
+    {
+        isArenaStarted = false;
+        DisableAllWaves();
+        ToggleDoor();
+    }
+
+    private void EnableCurrentWave()
+    {
+        waveList[currentWaveCount].SetActive(true);
+    }
+
+    private void DisableAllWaves()
+    {
+        foreach(GameObject wave in waveList)
+        {
+            wave.SetActive(false);
+        }
+    }
+
+    private void ToggleDoor()
+    {
+        if (doorList.Count == 0) Debug.Log($"Door Missing in Arena Script: {gameObject.name}");
+
+        foreach(GameObject door in doorList)
+        {
+            try
+            {
+                // Animator string name is not final nor if it is a trigger/boolean
+                door.GetComponent<Animator>().SetTrigger("function");
+            }
+            catch
+            {
+                Debug.LogError($"{door.name} animator function cannot be found || has an error with trigger name");
+            }
+        }
+    }
+
+    private bool IsWaveFinished()
+    {
+        return waveList[currentWaveCount].GetComponent<Transform>().childCount == 0;
+    }
+
+    private bool IsArenaFinished()
+    {
+        return currentWaveCount == maxWave;
+    }
+}
