e9a2895df5f82b0c1cd89da3c32f659181d59445
Justin Tran <jhtran023@gmail.com>
2024-05-20 12:29:43 +1000

    Add checkpoints prefab. Remove Debug Logs

---
+++ DualWield/Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private CheckpointData checkPointData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkPointData = GameObject.FindGameObjectWithTag("Player Data").GetComponent<CheckpointData>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            checkPointData.UpdateCheckpoint(transform.position);
+        }
+    }
+}
