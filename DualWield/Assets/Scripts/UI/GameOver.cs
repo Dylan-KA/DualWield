6283bfde63d0a59c569e7499bb3614b12d17ff29
dadysherwin1 <86547852+dadysherwin1@users.noreply.github.com>
2024-05-25 19:06:56 +1000

    Game over is nicer

--- DualWield/Assets/Scripts/UI/GameOver.cs
+++ DualWield/Assets/Scripts/UI/GameOver.cs
@@ -2,16 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class GameOver : MonoBehaviour
 {
-    [SerializeField] GameObject gameOverMenu;
+    private UIDocument document;
+
+    private Button respawnButton;
+    private Button quitButton;
     [SerializeField] private PlayerCharacter playerCharacter;
+
+    [SerializeField] GameObject eventSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameOverMenu.SetActive(false);
+        document = GetComponent<UIDocument>();
+        document.rootVisualElement.visible = false;
+
+        respawnButton = document.rootVisualElement.Q("RespawnButton") as Button;
+        respawnButton.RegisterCallback<ClickEvent>(RespawnPlayer);
+        quitButton = document.rootVisualElement.Q("QuitButton") as Button;
+        quitButton.RegisterCallback<ClickEvent>(ReturnToMainMenu);
+        Time.timeScale = 1f;
+
         playerCharacter = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCharacter>();
     }
 
@@ -25,18 +39,18 @@ public class GameOver : MonoBehaviour
 
     private void SetGameOver()
     {
-        gameOverMenu.SetActive(true);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        eventSystem.SetActive(false);
+        document.rootVisualElement.visible = true;
+        playerCharacter.SetMouseLocked(false);
         Time.timeScale = 0f;
     }
-    public void RespawnPlayer()
+    public void RespawnPlayer(ClickEvent evt)
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 
-    public void ReturnToMainMenu()
+    public void ReturnToMainMenu(ClickEvent evt)
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
