186ea8e5aad4e66550aad8dcba23f75d9e3ab9ad
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2024-06-12 11:47:48 +1000

    Fixed Bug with UI staying on Door Cams

--- DualWield/Assets/Scripts/CameraManager.cs
+++ DualWield/Assets/Scripts/CameraManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private Camera PlayerCamera;
+    [SerializeField] private Camera WeaponCamera;
+    [SerializeField] private GameObject HealthBar;
+    [SerializeField] private GameObject AmmoBar;
+    [SerializeField] private GameObject CrossHair;
     [SerializeField] private float DoorCamSeconds;
     private GameObject CurrentDoorCamera;
 
@@ -25,12 +29,20 @@ public class CameraManager : MonoBehaviour
         DoorCamera.SetActive(true);
         CurrentDoorCamera = DoorCamera;
         PlayerCamera.enabled = false;
+        WeaponCamera.enabled = false;
+        HealthBar.SetActive(false);
+        AmmoBar.SetActive(false);
+        CrossHair.SetActive(false);
         Invoke("EndDoorCam", DoorCamSeconds);
     }
 
     private void EndDoorCam()
     {
         PlayerCamera.enabled = true;
+        WeaponCamera.enabled = true;
+        HealthBar.SetActive(true);
+        AmmoBar.SetActive(true);
+        CrossHair.SetActive(true);
         CurrentDoorCamera.SetActive(false);
     }
 }
